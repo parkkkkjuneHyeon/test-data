43ed26ac25947cbae068b010f14e14e96fec2543
박준현 <gktkwk91@naver.com>
2025-05-13 22:51:19 +0900

    [feat] 스프링 부트 프로젝트 초기화 인텔리 제이에서 Spring Initializer를 통해 프로젝트 초기화하고, 당장 필요할 것으로 예상되는 디펜던시를 추가함.2 #6

---
+++ src/test/java/com/junh/testdata/TestDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junh.testdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
